10874c0d594efafabc229ec47d3d57c43ff27809
fracs31 <pist.fracs95@gmail.com>
2023-03-22 17:56:02 +0100

    ADD comments

--- server.php
+++ server.php
@@ -26,7 +26,7 @@ if (isset($_POST['delete'])) {
 //Se si vuole cambiare lo stato di una task
 if (isset($_POST['status'])) {
     $index = $_POST['status']; //salvo l'indice della task
-    $todo_list[$index]["success"] = !$todo_list[$index]["success"];
+    $todo_list[$index]["success"] = !$todo_list[$index]["success"]; //cambio lo stato della task
     $new_todo_list = json_encode($todo_list); //trasformo l'array associativo in una stringa
     file_put_contents("./todo.json", $new_todo_list); //inserisco la stringa nel file json
     $todo_string = file_get_contents("./todo.json"); //prendo i dati da "todo.json"
