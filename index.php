e28da9f5859a06a0c91948fc8e6b831054214a16
fracs31 <pist.fracs95@gmail.com>
2023-03-22 15:23:46 +0100

    ADD style

--- index.php
+++ index.php
@@ -8,14 +8,61 @@
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- Axios -->
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/ae23fa42a2.js" crossorigin="anonymous"></script>
     <title>PHP Todo List JSON</title>
 </head>
 <body>
     <!-- App -->
     <div id="app">
-        
+        <!-- Main -->
+        <main class="main-content">
+            <!-- Container -->
+            <div class="container">
+                <!-- Contenuto -->
+                <div class="content">
+                    <!-- Titolo -->
+                    <h1 class="title">
+                        Todo List
+                    </h1>
+                    <!-- Todo List -->
+                    <ul class="todo-list">
+                        <!-- Task -->
+                        <li class="task">
+                            <!-- Nome della task -->
+                            <span class="task__name">Fare la lavatrice</span>
+                            <!-- Cancellazione della task -->
+                            <span class="task__delete"><i class="fa-solid fa-trash"></i></span>
+                        </li>
+                        <!-- Task -->
+                        <li class="task">
+                            <!-- Nome della task -->
+                            <span class="task__name">Fare la lavatrice</span>
+                            <!-- Cancellazione della task -->
+                            <span class="task__delete"><i class="fa-solid fa-trash"></i></span>
+                        </li>
+                        <!-- Task -->
+                        <li class="task">
+                            <!-- Nome della task -->
+                            <span class="task__name">Fare la lavatrice</span>
+                            <!-- Cancellazione della task -->
+                            <span class="task__delete"><i class="fa-solid fa-trash"></i></span>
+                        </li>
+                    </ul>
+                    <!-- Input -->
+                    <div class="input">
+                        <!-- Input -->
+                        <input class="text" type="text" placeholder="Inserisci elemento...">
+                        <!-- Bottone -->
+                        <button class="btn">Inserisci</button>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
     <!-- JavaScript -->
-    <script src="./app.js"></script>
+    <script src="./js/app.js"></script>
 </body>
 </html>
